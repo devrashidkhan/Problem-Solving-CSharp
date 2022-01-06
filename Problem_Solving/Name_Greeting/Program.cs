d43969f59f8fdec776af76ff4e5c30898a264f96
Rashedul Islam <rashidkhan71011@gmail.com>
2022-01-06 20:33:56 +0600

    NameGreeting problem solve

---
+++ Problem_Solving/Name_Greeting/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+NameGreeting name = new NameGreeting();
+Console.WriteLine(name.HelloName());
+
+public class NameGreeting
+{
+    public string name;
+
+    public string HelloName()
+    {
+        Console.Write("Type a name : ");
+        name = Console.ReadLine();
+        return "Hello " + name + '!';
+    }
+}
