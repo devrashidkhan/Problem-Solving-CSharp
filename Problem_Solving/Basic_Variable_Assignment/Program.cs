6a1bdeafd5e6bced9041f5504260d4fd236f3a3b
Rashedul Islam <rashidkhan71011@gmail.com>
2022-01-06 20:15:30 +0600

    Basic Variable Assignment solve this program.

---
+++ Problem_Solving/Basic_Variable_Assignment/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+VariableAssignment name = new VariableAssignment();
+Console.WriteLine(name.nameConcat(Console.ReadLine()));
+
+public class VariableAssignment
+{
+    public string nameConcat(string name)
+    {
+        return (name + "Edabit");
+    }
+}
