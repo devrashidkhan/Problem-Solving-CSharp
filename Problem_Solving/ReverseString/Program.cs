c9e1c89e10ce2b5add880828246d60ee6aae70e3
Rashedul Islam <rashidkhan71011@gmail.com>
2022-01-07 12:13:55 +0600

    ReverseString Program added

---
+++ Problem_Solving/ReverseString/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Plase Your Name : ");
+string name = Console.ReadLine();
+Console.WriteLine(StringHelper.ReverseString(name));
+static class StringHelper
+{ 
+    public static string ReverseString(string myStr)
+    {
+        char[] chars = myStr.ToCharArray();
+        Array.Reverse(chars);
+        return new string(chars);
+    }
+}
