7520447aff4e65dc8e5131488d1275162cf07411
Rashedul Islam <rashidkhan71011@gmail.com>
2022-01-18 12:30:55 +0600

    All solve

---
+++ Problem_Solving/BeautifulMatrix_263A/Program.cs
@@ -0,0 +1,12 @@
+﻿int x = 0;
+for (int i = 1; i <= 5; ++i)
+{
+    for (int j = 1; j <= 5; ++j)
+    {
+        
+        if (x == 1)
+        {
+            Console.WriteLine(x(i - 3) + x(j - 3));
+        }
+    }
+}
---
+++ Problem_Solving/Petya_an_Strings_112A/Program.cs
@@ -0,0 +1,30 @@
+﻿string s = Console.ReadLine();
+
+string str = Console.ReadLine();
+
+string lower = s.ToLower();
+
+string lower1 = str.ToLower();
+
+int i;
+
+if (lower == lower1)
+{
+    Console.WriteLine("0");
+}
+else
+{
+    for (i = 0; i < lower.Length; i++)
+    {
+        if (lower[i] < lower1[i])
+        {
+            Console.WriteLine("-1");
+            break;
+        }
+        if (lower[i] > lower1[i])
+        {
+            Console.WriteLine("1");
+            break;
+        }
+    }
+}
