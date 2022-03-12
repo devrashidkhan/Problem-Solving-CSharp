198284ca0f8a5d7e78613a6953a9e6e8ba35e348
Rashedul Islam <rashidkhan71011@gmail.com>
2022-03-12 20:51:25 +0600

    Word solve

---
+++ Problem_Solving/Test/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long m, n, a, x, y;
+            m = Convert.ToInt32(Console.ReadLine());
+            n = Convert.ToInt32(Console.ReadLine());
+            a = Convert.ToInt32(Console.ReadLine());
+            if (m % a == 0)
+            {
+                x = m / a;
+            }
+            else
+            {
+                x = m / a + 1;
+            }
+
+            if (n % a == 0)
+            {
+                y = n / a;
+            }
+
+            else
+            {
+                y = n / a + 1;
+            }
+            Console.WriteLine(x * y);
+        }
+    }
+}
---
+++ Problem_Solving/TheatreSquare_1A/Program.cs
@@ -0,0 +1,16 @@
+﻿long m, n, a, x, y;
+m = int.Parse(Console.ReadLine());
+n = int.Parse(Console.ReadLine());
+a = int.Parse(Console.ReadLine());
+
+if (m % a == 0)
+    x = m / a;
+else
+    x = m / a + 1;
+
+if(n% a == 0)
+    y = n / a;
+else
+    y = n / a + 1;
+
+Console.WriteLine(x*y);
